74d1b78804c05cf0405b978797ff42e703e72548
Adrians Skapars <adrians.skapars@student.manchester.ac.uk>
2026-06-20 22:07:22 +0100

    Add BLOOM_INPUT_SEARCH env override (enable BEAST input search)

--- experiments/bloom/bloom_beast_tree_corrupt.py
+++ experiments/bloom/bloom_beast_tree_corrupt.py
@@ -7322,6 +7322,8 @@ if __name__ == "__main__":
         cfg.setdefault("refinement", {})["skip_finished"] = os.environ["BLOOM_SKIP_FINISHED"].lower() in ("1", "true", "yes")
     if os.environ.get("BLOOM_CORRUPTION_ENABLED") is not None and os.environ.get("BLOOM_CORRUPTION_ENABLED") != "":
         cfg.setdefault("corruption_output", {})["enabled"] = os.environ["BLOOM_CORRUPTION_ENABLED"].lower() in ("1", "true", "yes")
+    if os.environ.get("BLOOM_INPUT_SEARCH") is not None and os.environ.get("BLOOM_INPUT_SEARCH") != "":
+        cfg.setdefault("input_search", {})["enabled"] = os.environ["BLOOM_INPUT_SEARCH"].lower() in ("1", "true", "yes")
     if any(os.environ.get(k) for k in ("BLOOM_FOLDER", "BLOOM_TARGET_FLOOR", "BLOOM_BETA", "BLOOM_MIN_P", "BLOOM_ABS_FLOOR", "BLOOM_MAX_TURNS", "BLOOM_NUM_ROUNDS", "BLOOM_FREEZE_INPUT", "BLOOM_SKIP_FINISHED", "BLOOM_CORRUPTION_ENABLED")):
         _co = cfg.get("corruption_output", {})
         _rf = cfg.get("refinement", {})
